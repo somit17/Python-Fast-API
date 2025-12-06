cd26d0c4168aa3b747553f6e2c75ef796bc4ea30
Somit17 <somitmeshram3256@gmail.com>
2025-12-06 21:48:05 +0530

    Numbers and Operations Basics

---
+++ Numeric/numbers.py
@@ -0,0 +1,26 @@
+num = 3.14
+print(type(num))
+
+
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2 -
+# Modulus:        3 % 2 -- Gets Remainder
+
+
+num = 1
+num *=12
+print(num)
+
+#Info about method
+print(help(abs))
+
+print('Operations - Round')
+print(round(3.75)) #4
+print(round(3.15)) #3
+print(round(3.16,1)) #4
+print(help(round))
---
+++ Numeric/numericComparison.py
@@ -0,0 +1,18 @@
+# Comparisons:
+num_1 = 3
+num_2 = 2
+# Equal:            3 == 2
+print(num_1 == num_2)
+
+# Not Equal:        3 != 2
+print(num_1 != num_2)
+# Greater Than:     3 > 2
+print(num_1 > num_2)
+# Less Than:        3 < 2
+print(num_1<num_2)
+# Greater or Equal: 3 >= 2
+print(num_1>=num_2)
+# Less or Equal:    3 <= 2
+print(num_1<=num_2)
+
+
